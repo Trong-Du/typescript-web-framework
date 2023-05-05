2c2c97617c1662137cc639f0411262c57b982881
DuNT <trongdu94@gmail.com>
2023-05-05 22:55:12 +0700

    implement Attributes, Eventing and Sync class

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
-import axios from 'axios';
 import { User } from './models/User';
 
-const user = new User({ name: 'new record', age: 30 });
+const user = new User({ id: 1, name: 'new name', age: 30 });
+
+user.on('change', () => {
+  console.log(user);
+});
+
+user.on('save', () => {
+  console.log(user);
+});
 
 user.save();
