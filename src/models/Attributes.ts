02c1072eb89043e613d3c1035891df071ee8d8d9
DuNT <trongdu94@gmail.com>
2023-05-06 10:43:32 +0700

    refactor User model

--- src/models/Attributes.ts
+++ src/models/Attributes.ts
@@ -1,4 +1,4 @@
-export class Attribbutes<T> {
+export class Attributes<T> {
   constructor(private data: T) {}
 
   get = <K extends keyof T>(key: K): T[K] => {
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,7 +1,7 @@
-import axios, { AxiosResponse } from 'axios';
-import { Attribbutes } from './Attributes';
+import { ApiSync } from './ApiSync';
+import { Attributes } from './Attributes';
 import { Eventing } from './Eventing';
-import { Sync } from './Sync';
+import { Model } from './Model';
 
 export interface IUserProps {
   id?: number;
@@ -11,52 +11,12 @@ export interface IUserProps {
 
 const rootUrl = 'http://localhost:3000/users';
 
-export class User {
-  public events: Eventing = new Eventing();
-  public sync: Sync<IUserProps> = new Sync<IUserProps>(rootUrl);
-  public attributes: Attribbutes<IUserProps>;
-
-  constructor(attrs: IUserProps) {
-    this.attributes = new Attribbutes<IUserProps>(attrs);
-  }
-
-  get on() {
-    return this.events.on;
-  }
-
-  get trigger() {
-    return this.events.trigger;
-  }
-
-  get get() {
-    return this.attributes.get;
-  }
-
-  set(update: IUserProps): void {
-    this.attributes.set(update);
-    this.events.trigger('change');
-  }
-
-  fetch(): void {
-    const id = this.get('id');
-
-    if (typeof id !== 'number') {
-      throw new Error('Cannot fetch without an id');
-    }
-
-    this.sync.fetch(id).then((response: AxiosResponse): void => {
-      this.set(response.data);
-    });
-  }
-
-  save(): void {
-    this.sync
-      .save(this.attributes.getAll())
-      .then((response: AxiosResponse) => {
-        this.trigger('save');
-      })
-      .catch(() => {
-        this.trigger('error');
-      });
+export class User extends Model<IUserProps> {
+  static buildUser(attrs: IUserProps): User {
+    return new User(
+      new Attributes<IUserProps>(attrs),
+      new Eventing(),
+      new ApiSync<IUserProps>(rootUrl)
+    );
   }
 }
